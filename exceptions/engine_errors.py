fd83c6f9de0a0bb09d386a02af8d36220088dd15
Mateusz Jabłoński <mjablonski92@gmail.com>
2016-02-27 23:01:50 +0100

    Invalid language name error

---
+++ exceptions/engine_errors.py
@@ -0,0 +1,14 @@
+
+class InvalidLanguageNameError(Exception):
+
+    def __init__(self, name, available):
+        self.name = name
+        self.available = available
+
+    def __str__(self):
+        string = str.format("Kernel for language '{0}' not found. Available languages: \n", self.name)
+
+        for name in self.available:
+            string += str.format("\t'{0}'\n", name)
+
+        return string
--- kernel_engine.py
+++ kernel_engine.py
@@ -1,5 +1,32 @@
+from jupyter_client import MultiKernelManager
+from jupyter_client.kernelspec import KernelSpecManager
+
 
 class KernelEngine:
 
-    def execute(self, language, code, context=None):
+    def __init__(self):
+        spec_manager = KernelSpecManager()
+        kernel_names = spec_manager.find_kernel_specs()
+
+        self.available_kernel_names_mappings = {}
+
+        for name in kernel_names:
+            spec = spec_manager.get_kernel_spec(name)
+            self.available_kernel_names_mappings[spec.display_name] = name
+
+        print(self.available_kernel_names_mappings)
+
+        self.manager = MultiKernelManager()
+
+    def __del__(self):
+        # TODO
         pass
+
+    def get_kernel(self, language):
+        pass
+
+    def get_client(self, language, context=None):
+        pass
+
+    def execute(self, language, code, context=None):
+        return ''
