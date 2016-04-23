04cb9fa8dc71b11acbd896aac9c7fb775cbb1cee
Mateusz Jabłoński <mjablonski92@gmail.com>
2016-04-23 21:41:58 +0200

    Empty LaTeX and Markdown processors

---
+++ processors/latex_processor.py
@@ -0,0 +1,7 @@
+from processors.processor import Processor as BaseProcessor
+
+
+class Processor(BaseProcessor):
+    """LaTeX Processor. Base class for user-defined LaTeX processors"""
+    # TODO
+    pass
---
+++ processors/markdown_processor.py
@@ -0,0 +1,7 @@
+from processors.processor import Processor as BaseProcessor
+
+
+class Processor(BaseProcessor):
+    """Markdown Processor. Base class for user-defined Markdown processors"""
+    # TODO
+    pass
