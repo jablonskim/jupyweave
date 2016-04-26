df0c239a23fa5078972688050e8cdb8da3c488cc
Mateusz Jablonski <mjablonski92@gmail.com>
2016-04-26 16:27:33 +0200

    Markdown processor

--- examples/example1.md
+++ examples/example1.md
@@ -1,6 +1,6 @@
 Markdown example
 ================
-
+<!!![Python 3] timeout=5000>
 Testtesttest
 ------------
 
@@ -22,6 +22,23 @@ T**es**t
 > qqq
 
 Link [example](https://pl.wikipedia.org/wiki/Markdown)
+
+<#>
+%matplotlib inline
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return x**2
+
+x = np.linspace(0, 3*np.pi, 500)
+plt.plot(x, np.sin(f(x)))
+plt.title('A simple chirp')
+print('before')
+plt.show()
+print('after')
+<@>
 
 Test `Test` Test
 
--- processors/markdown_processor.py
+++ processors/markdown_processor.py
@@ -3,5 +3,20 @@ from processors.processor import Processor as BaseProcessor
 
 class Processor(BaseProcessor):
     """Markdown Processor. Base class for user-defined Markdown processors"""
-    # TODO
-    pass
+
+    def source(self, code):
+        """Processing source code"""
+        return '```\n' + code + '\n```\n'
+
+    def text(self, text):
+        """Processing text results of code execution"""
+        return text.replace('\n', '  \n')
+
+    def image(self, data, mime_type):
+        """Processing image"""
+        url = super(Processor, self).image(data, mime_type)
+        return '\n![](%s)  \n' % url
+
+    def result(self, result):
+        """Processing whole result"""
+        return '\n\n' + result + '\n\n'
